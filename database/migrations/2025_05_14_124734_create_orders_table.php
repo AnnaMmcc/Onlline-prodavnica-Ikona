8423abef2fba9c56a114a5b33c0b260fa3caf2dc
AnnaMmcc <urosevicanja913@gmail.com>
2025-05-14 16:59:27 +0200

    *Created Orders table

--- database/migrations/2025_05_14_124734_create_orders_table.php
+++ database/migrations/2025_05_14_124734_create_orders_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('status')->default('pending');
+            $table->decimal('total_price', 8, 2);
+            $table->string('payment_method');
             $table->timestamps();
+
         });
     }
 
