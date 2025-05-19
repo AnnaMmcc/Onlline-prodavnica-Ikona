155ff06170bbab25641d4668c2a9782f3dddffd0
AnnaMmcc <urosevicanja913@gmail.com>
2025-05-19 16:14:21 +0200

    *Added phone column to Users

--- resources/views/orders/card.blade.php
+++ resources/views/orders/card.blade.php
@@ -3,9 +3,18 @@
     Placanje/racun-Ikonopisna radionica-Andjel Sevic
 @endsection
 @section("Sadrzaj")
-    Hvala Sto kupujete kod nas!
-    Uplatu mozete izvrsiti na racun: XXXX-XXXXXX-XXX-XXX
-    Kontaktiracemo Vas radi potvrde porudzbine!
+    <div class="container">
+        Hvala Vam na poverenju. Molimo ostavite kontakt radi potvrde narudzbine:
+        <form class="col-lg-6 m-3">
+            <div class="form-group">
+                <input class="form-control" type="number" placeholder="Unesite Vas broj telefona.." name="telefon" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Potvrdi</button>
+        </form>
+        Kontaktiracemo Vas radi potvrde porudzbine!
+    </div>
+
 @endsection
 
 
--- resources/views/orders/cash.blade.php
+++ resources/views/orders/cash.blade.php
@@ -1,1 +1,10 @@
-Kesssssssssssss
+@extends('layout')
+@section('Naslov')
+    Placanje/Pouzecem-Ikonopisna radionica-Andjel Sevic
+@endsection
+@section('Sadrzaj')
+    <div class="container text-center d-flex flex-wrap justify-center align-ceneter">
+        Hvala Vam na poverenju. Molimo ostavite kontakt telefon radi potvrde porudzbine:
+        <input type="number" name="telefon" placeholder="Unesite Vas broj telefona.." required>
+    </div>
+@endsection
