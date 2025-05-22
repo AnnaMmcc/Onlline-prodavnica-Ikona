112b2f524a514b1ab381d618b60ca7eb4eb1fcc7
AnnaMmcc <urosevicanja913@gmail.com>
2025-05-22 09:08:46 +0200

    *Added footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-secondary text-center text-white py-1 mt-auto">
+    <div class="container">Ikonopisna radionica Andjel Sevic &copy;2025</div>
+</footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>@yield("Naslov")</title>
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+        main {
+            flex: 1;
+        }
+    </style>
 </head>
 <body>
 @include("navigation")
 @yield("Sadrzaj")
+@include("footer")
 </body>
 </html>
