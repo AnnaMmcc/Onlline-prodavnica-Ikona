d9fd9eff0f540d6803eb38b0d5e735efb0bc36b3
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-09 14:54:20 +0200

    *Changed word to cyrilic

--- resources/views/cash.blade.php
+++ resources/views/cash.blade.php
@@ -9,7 +9,7 @@
 <div class="container text-center m-3">
     <h2><strong>Плаћање поузећем</strong></h2>
     <p>Хвала на поруџбини!</p>
-    <p>Iznos: <strong>{{ $order->total_price }} РСД</strong></p>
+    <p>Износ: <strong>{{ $order->total_price }} РСД</strong></p>
     <p>Контактираћемо Вас ради потврде поруџбине. Поруџбина стиже у року од 3-5 радних дана.Платите куриру приликом доставе наведени износ.</p>
     <a href="/" class="btn btn-warning w-100 text-white fw-bold">Врати ме на почетну</a>
 </div>
