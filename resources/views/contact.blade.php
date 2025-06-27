3810a89b6d55a44fa686239519755a761c4d11b4
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-27 21:49:50 +0200

    *Added map

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -27,9 +27,19 @@
                                 <i class="fa-brands fa-instagram text-brown me-2"></i>
                                 <a href="https://www.instagram.com/andjelandjel" target="_blank" class="text-dark text-decoration-none">@andjelandjel</a>
                             </li>
+                            <li class="mb-3">
+                                <i class="fa-solid fa-location-dot text-brown me-2"></i>
+                                <span class="text-dark text-decoration-none">Краља Петра првог 9, Инђија</span>
+                            </li>
                         </ul>
 
+                        <div class="map-responsive" style="overflow:hidden; padding-bottom:56.25%; position:relative; height:0;">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2818.8928714755207!2d20.07922977542685!3d45.047395761153226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475af8bf3a571939%3A0x6fcc296e2e6a5374!2z0JrRgNCw0ZnQsCDQn9C10YLRgNCwIEkg0JrQsNGA0LDRktC-0YDRktC10LLQuNGb0LAgOSwg0JjQvdGS0LjRmNCwIDIyMzIw!5e0!3m2!1ssr!2srs!4v1751052829633!5m2!1ssr!2srs"
+                                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
                         <p class="mt-4">Одговарамо у најкраћем могућем року. Хвала вам на поверењу! 🕊️</p>
+
                     </div>
                 </div>
             </div>
