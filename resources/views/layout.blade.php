f21bbb20a2a6f7b709ec3687f8d49e8e8233d44c
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-30 10:20:28 +0200

    *Added favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
-
-
-
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" href="{{ asset('storage/images/1000000645.jpg') }}" type="image/jpeg">
     <title>@yield("Naslov")</title>
 </head>
 <body class="d-flex flex-column min-vh-100 brown-bg text-white">
