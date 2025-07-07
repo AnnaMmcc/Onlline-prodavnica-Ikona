e5be4fb6f0c72ae8a75df685dbcfddd8a3871f19
AnnaMmcc <urosevicanja913@gmail.com>
2025-07-07 12:45:29 +0200

    *Added meta tittles and descriptions, og tags

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,13 @@
     <title>@yield('title', 'Ikonopisna radionica Anđel Sević - Online prodaja ikona')</title>
 
     <meta name="description" content="@yield('meta_description', 'Kupite unikatne pravoslavne ikone iz radionice Anđel Sević. Ručno rađene i dostupne online.')">
+
+    <meta property="og:title" content="@yield('title', 'Ikonopisna radionica Anđel Sević')">
+    <meta property="og:description" content="@yield('meta_description', 'Kupite pravoslavne, ručno rađene ikone.')">
+    <meta property="og:image" content="@yield('meta_image', asset('images/default-og.jpg'))">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- resources/views/permalink.blade.php
+++ resources/views/permalink.blade.php
@@ -2,9 +2,13 @@
 @section("Naslov")
     Икона|Иконописна радионица-Анђел Шевић
 @endsection
-@section('title', 'Galerija ikona - Ikonopisna radionica Anđel Sević - Ikona -'. $id->name)
+@section('title', 'Galerija ikona - Ikonopisna radionica Anđel Šević - Ikona - ' . $id->name)
 
-@section('meta_description', 'Pogledajte našu bogatu galeriju pravoslavnih ikona. Svaka ikona ručno rađena sa posebnom pažnjom.'. $id->description)
+@section('meta_description', 'Pogledajte našu bogatu galeriju pravoslavnih ikona. Svaka ikona ručno rađena sa posebnom pažnjom. ' . \Illuminate\Support\Str::limit(strip_tags($id->description), 150))
+
+@section('meta_image', asset('storage/' . $id->image))
+
+
 @section("Sadrzaj")
     <div class="container d-flex justify-content-center align-items-center mt-3">
         <div class="card w-100 w-sm-75 w-md-50 mx-auto">
