4ed66dd83ec2833aeda95be6a3ce618c5d3990b7
AnnaMmcc <urosevicanja913@gmail.com>
2025-07-12 21:06:09 +0200

    *Fixed table bug

--- database/migrations/2025_07_12_190226_create_order_items_table.php
+++ database/migrations/2025_07_12_190226_create_order_items_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('restrict');
 
             $table->integer('quantity')->default(1);
             $table->decimal('price', 8, 2);
