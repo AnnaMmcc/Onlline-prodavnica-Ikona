deb95ea86e33adfd6cf2351efb2628d4aecd169d
AnnaMmcc <urosevicanja913@gmail.com>
2025-07-13 14:19:37 +0200

    *Changed head section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,22 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta name="description" content="@yield('meta_description', 'Kupite unikatne pravoslavne ikone iz radionice Anđel Šević. Ručno rađene i dostupne online.')">
-
     <meta property="og:title" content="@yield('title', 'Ikonopisna radionica Anđel Sević')">
     <meta property="og:description" content="@yield('meta_description', 'Kupite pravoslavne, ručno rađene ikone.')">
     <meta property="og:image" content="@yield('meta_image', asset('images/default-og.jpg'))">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="website">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <link rel="icon" href="{{ asset('storage/images/1000000645.jpg') }}" type="image/jpeg">
-    <title>@yield("Naslov")</title>
+
+    <title>@yield('Naslov', 'Ikonopisna radionica Anđel Sević')</title>
 </head>
+
 <body class="d-flex flex-column min-vh-100 brown-bg text-white">
 
 @include('navigation')
@@ -34,6 +37,9 @@
 </main>
 
 @include('footer')
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
 </body>
 </html>
 
