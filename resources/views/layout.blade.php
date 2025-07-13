dad4fc92604aa38dbc32d0523c7aa976c03d48cc
AnnaMmcc <urosevicanja913@gmail.com>
2025-07-13 22:17:53 +0200

    *Changed head section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -39,7 +39,7 @@
 @include('footer')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
+{{--test--}}
 </body>
 </html>
 
