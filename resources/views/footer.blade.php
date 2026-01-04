5c562930bf7996098fc9dea1fb46e0921eae6437
AnnaMmcc <urosevicanja913@gmail.com>
2026-01-04 21:41:42 +0100

    *New design

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,27 +1,35 @@
-<footer class="text-center py-5 mt-auto light-bg">
-    <div class="container">
-        <p class="mb-3">Иконописна радионица Анђел Шевић &copy; 2025</p>
+<footer class="footer-modern py-5 mt-auto">
+    <div class="container text-center text-md-start">
+        <div class="row align-items-center">
 
-        <div class="d-flex justify-content-center gap-4 fs-4">
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5 class="footer-logo">Иконописна радионица</h5>
+                <h4 class="footer-brand-name">Анђел Шевић</h4>
+                <div class="footer-divider mx-auto mx-md-0"></div>
+            </div>
 
-            <a href="https://www.instagram.com/andjelandjel" target="_blank" class="text-dark">
-                <i class="fa-brands fa-instagram text-white"></i>
-            </a>
+            <div class="col-md-4 mb-4 mb-md-0 text-center">
+                <p class="mb-1 footer-text">Свака икона је молитва у боји.</p>
+                <small class="footer-copy">&copy; 2026 Сва права задржана.</small>
+            </div>
 
+            <div class="col-md-4">
+                <div class="d-flex justify-content-center justify-content-md-end gap-3">
+                    <a href="https://www.instagram.com/andjelandjel" target="_blank" class="footer-icon-link" title="Instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="mailto:sevicandjel@gmail.com" class="footer-icon-link" title="Email">
+                        <i class="fa-regular fa-envelope"></i>
+                    </a>
+                    <a href="tel:+381638994803" class="footer-icon-link" title="Pozovi">
+                        <i class="fa-solid fa-phone-volume"></i>
+                    </a>
+                    <a href="/contact" class="footer-icon-link" title="Kontakt">
+                        <i class="fa-regular fa-address-card"></i>
+                    </a>
+                </div>
+            </div>
 
-            <a href="mailto:sevicandjel@gmail.com" class="text-dark">
-                <i class="fa-regular fa-envelope text-white"></i>
-            </a>
-
-
-            <a href="tel:+381638994803" class="text-dark">
-                <i class="fa-solid fa-phone-volume text-white"></i>
-            </a>
-
-
-            <a href="/contact" class="text-dark">
-                <i class="fa-regular fa-address-card text-white"></i>
-            </a>
         </div>
     </div>
 </footer>
--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,60 +1,53 @@
-<nav class="navbar navbar-expand-lg navbar-light brown-bg">
+<nav class="navbar navbar-expand-lg navbar-dark custom-nav py-3">
     <div class="container-fluid">
-
-        <a class="navbar-brand brand-logo text-white d-flex align-items-center gap-2" href="{{ url('/') }}">
-            <img src="{{ asset('storage/images/1000000645.jpg') }}" alt="Лого" style="height: 60px;">
-            <span>
-        Анђел Шевић<br>
-        Иконописна<br>
-        радионица
-    </span>
+        <a class="navbar-brand d-flex align-items-center gap-2" href="{{ url('/') }}">
+            <img src="{{ asset('storage/images/1000000645.jpg') }}" alt="Лого" class="nav-logo-img">
+            <div class="brand-text-wrapper">
+                <span class="brand-title">АНЂЕЛ ШЕВИЋ</span>
+                <span class="brand-subtitle">ИКОНОПИСНА РАДИОНИЦА</span>
+            </div>
         </a>
 
-        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExtra" aria-controls="navbarExtra" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExtra">
             <span class="toggler-text">Мени <i class="fas fa-chevron-down"></i></span>
         </button>
 
-
         <div class="collapse navbar-collapse" id="navbarExtra">
-
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link text-white" href="/">Почетна</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="/about">О нама</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="/shop">Иконе - Продавница</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="/contact">Контакт</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="/cart">Корпа <i class="fa-solid fa-cart-shopping"></i></a></li>
+                <li class="nav-item"><a class="nav-link nav-custom-link" href="/">Почетна</a></li>
+                <li class="nav-item"><a class="nav-link nav-custom-link" href="/about">О нама</a></li>
+                <li class="nav-item"><a class="nav-link nav-custom-link" href="/shop">Продавница</a></li>
+                <li class="nav-item"><a class="nav-link nav-custom-link" href="/contact">Контакт</a></li>
+                <li class="nav-item"><a class="nav-link nav-custom-link" href="/cart">Корпа <i class="fa-solid fa-cart-shopping"></i></a></li>
             </ul>
 
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
                 @if(\Illuminate\Support\Facades\Auth::check())
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-warning text-white fw-bold" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Одјава</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
+                    @if(auth()->user()->role === 'admin')
+                        <li class="nav-item"><a class="nav-link nav-custom-link small" href="{{ route('product.new') }}">Додај</a></li>
+                        <li class="nav-item"><a class="nav-link nav-custom-link small" href="/admin/products/all">Уреди</a></li>
+                        <li class="nav-item"><a class="nav-link nav-custom-link small" href="{{ route('all.contacts') }}">Контакти</a></li>
+                        <li class="nav-item"><a class="nav-link nav-custom-link small" href="{{ route('all.orders') }}">Наруџбине</a></li>
+                    @endif
+
+                    <li class="nav-item dropdown ms-lg-3">
+                        <a class="nav-link dropdown-toggle auth-dropdown text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end shadow-lg">
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Профил</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Одјава</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                            </li>
+                        </ul>
                     </li>
                 @else
                     <div class="d-flex gap-2">
-                        <a class="btn btn-warning fw-bold" href="{{ route('login', ['redirect' => url()->current()]) }}">Пријави се</a>
-                        <a class="btn btn-warning fw-bold" href="{{ route('register', ['redirect' => url()->current()]) }}">
-                            Региструј се
-                        </a>
+                        <a class="btn btn-nav-gold" href="{{ route('login', ['redirect' => url()->current()]) }}">Пријави се</a>
+                        <a class="btn btn-nav-gold" href="{{ route('register', ['redirect' => url()->current()]) }}">Региструј се</a>
                     </div>
-                @endif
-
-                @if(auth()->check() && auth()->user()->role === 'admin')
-                    <li class="nav-item"><a class="nav-link text-white" href="{{ route('product.new') }}">Додај икону</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="/admin/products/all">Уреди | Обриши</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="{{ route('all.contacts') }}">Контакти</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="{{ route('all.orders') }}">Нарџбине</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-                            {{ Auth::user()->name }}
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Профил</a></li>
-                        </ul>
-                    </li>
                 @endif
             </ul>
         </div>
