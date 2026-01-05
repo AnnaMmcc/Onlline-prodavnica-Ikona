a8ff4f353a368d842ae681515c4cb703c06f3f50
AnnaMmcc <urosevicanja913@gmail.com>
2026-01-05 22:01:49 +0100

    *Fixed css bug

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -10,7 +10,7 @@ class HomePageController extends Controller
     public function index()
     {
         $availableIcons = IconsModel::where('is_available', 1)->latest()->get();
-        $unavailableIcons = IconsModel::where('is_available', 0)->latest()->paginate(12);
+        $unavailableIcons = IconsModel::where('is_available', 0)->latest()->paginate(6);
 
         return view("welcome", compact('availableIcons', 'unavailableIcons'));
     }
