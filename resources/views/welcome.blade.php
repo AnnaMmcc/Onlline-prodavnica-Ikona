d808256b476fd2bd53d9f92e9db0af771121f6b0
AnnaMmcc <urosevicanja913@gmail.com>
2026-05-29 15:11:46 +0200

    *Changed hero image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,27 +7,13 @@
 
 @section("Sadrzaj")
     <div class="container d-flex flex-column align-items-center justify-content-start mt-1">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="{{ asset('storage/images/za sajt_8ac40d91-1255-47b2-863e-f8c32c1414b6.webp') }}" class="d-block w-100" alt="Andjel Sevic slika">
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('storage/images/САјт%202_e36a3221-3251-4db3-b2cf-c17313f765b0~2.webp') }}" class="d-block w-100" alt="Isus Hristos ikona - citat">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+        <div class="hero-image-wrapper">
+
+    <img src="{{ asset('storage/images/za sajt_8ac40d91-1255-47b2-863e-f8c32c1414b6.webp') }}"
+         class="hero-image"
+         alt="Иконописна радионица Анђел Шевић">
+
+</div>
 
               <div class="container my-5">
 
